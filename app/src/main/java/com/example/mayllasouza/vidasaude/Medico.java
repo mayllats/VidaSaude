68598d2537f4f822176e1a400c6181f1fe2a120b
mayllats <mayllats@gmail.com>
2018-12-09 14:47:25 -0200

    Classes especialidade e medico

---
+++ app/src/main/java/com/example/mayllasouza/vidasaude/Medico.java
@@ -0,0 +1,42 @@
+package com.example.mayllasouza.vidasaude;
+
+public class Medico {
+
+        private String iD;
+        private String mEDNOME;
+        private String mEDCRM;
+        private String fKESP;
+
+        public String getID() {
+            return iD;
+        }
+
+        public void setID(String iD) {
+            this.iD = iD;
+        }
+
+        public String getMEDNOME() {
+            return mEDNOME;
+        }
+
+        public void setMEDNOME(String mEDNOME) {
+            this.mEDNOME = mEDNOME;
+        }
+
+        public String getMEDCRM() {
+            return mEDCRM;
+        }
+
+        public void setMEDCRM(String mEDCRM) {
+            this.mEDCRM = mEDCRM;
+        }
+
+        public String getFKESP() {
+            return fKESP;
+        }
+
+        public void setFKESP(String fKESP) {
+            this.fKESP = fKESP;
+        }
+
+    }
