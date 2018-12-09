329bd63df58b98d855b9cc375fb91eeaeea54867
mayllats <mayllats@gmail.com>
2018-12-09 17:17:16 -0200

    Botao voltar na tela final

--- app/src/main/java/com/example/mayllasouza/vidasaude/FinalActivity.java
+++ app/src/main/java/com/example/mayllasouza/vidasaude/FinalActivity.java
@@ -1,13 +1,27 @@
 package com.example.mayllasouza.vidasaude;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class FinalActivity extends AppCompatActivity {
+public class FinalActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+    }
+
+
+    public void voltarPrincipal(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        voltarPrincipal(null);
     }
 }
