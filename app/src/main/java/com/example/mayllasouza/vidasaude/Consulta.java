23ce06698a29f076a8f33395f4e0b86be61b12fc
mayllats <mayllats@gmail.com>
2018-12-09 14:52:37 -0200

    classe consulta

---
+++ app/src/main/java/com/example/mayllasouza/vidasaude/Consulta.java
@@ -0,0 +1,52 @@
+package com.example.mayllasouza.vidasaude;
+
+public class Consulta {
+
+    private String nomePaciente;
+
+    private String telefonePaciente;
+
+    private String dataPretendida;
+
+    private String horarioPretendido;
+
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public String getTelefonePaciente() {
+        return telefonePaciente;
+    }
+
+    public void setTelefonePaciente(String telefonePaciente) {
+        this.telefonePaciente = telefonePaciente;
+    }
+
+    public String getDataPretendida() {
+        return dataPretendida;
+    }
+
+    public void setDataPretendida(String dataPretendida) {
+        this.dataPretendida = dataPretendida;
+    }
+
+    public String getHorarioPretendido() {
+        return horarioPretendido;
+    }
+
+    public void setHorarioPretendido(String horarioPretendido) {
+        this.horarioPretendido = horarioPretendido;
+    }
+
+    public Consulta(String nomePaciente, String telefonePaciente, String dataPretendida, String horarioPretendido){
+        this.nomePaciente = nomePaciente;
+        this.telefonePaciente = telefonePaciente;
+        this.dataPretendida = dataPretendida;
+        this.horarioPretendido = horarioPretendido;
+    }
+}
